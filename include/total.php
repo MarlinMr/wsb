9f59d6a9f5714f5e74cad04d59528c02b1fe5e30
MarlinMr <51798819+MarlinMr@users.noreply.github.com>
2021-01-16 12:36:03 +0100

    Update total.php

--- include/total.php
+++ include/total.php
@@ -1,1 +1,16 @@
-
+<?php
+SELECT "SUM(portfolio) AS total FROM stocks WHERE stock =?";
+require "connhandler.php";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $stockid);
+$imgid = $_GET["stock"];
+$stmt->execute();
+$result = $stmt->get_result();
+$result = $stmt->get_result();
+while ($row = $result->fetch_array(MYSQLI_NUM)){
+  foreach ($row as $r){
+    print "$r ";
+  }
+}
+$conn->close();
+?>
