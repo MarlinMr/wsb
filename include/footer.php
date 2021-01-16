22ec8b87897dd5802a8ec442fb6dd39984475ea2
MarlinMr <51798819+MarlinMr@users.noreply.github.com>
2021-01-16 13:13:50 +0100

    Update footer.php

--- include/footer.php
+++ include/footer.php
@@ -1,5 +1,7 @@
 <html>
-To delete your data from the database, simply set your stock to 0.
-Contribute at <a href="https://github.com/MarlinMr/wsb">https://github.com/MarlinMr/wsb</a>.
-Donate by buying more stock.
+  <p>
+    To delete your data from the database, simply set your stock to 0.
+    Contribute at <a href="https://github.com/MarlinMr/wsb">https://github.com/MarlinMr/wsb</a>.
+    Donate by buying more stock.
+  </p>
 </html>
