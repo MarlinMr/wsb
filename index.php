5304e36dfdd8b809ff3816e4af78e7325193b78b
MarlinMr <51798819+MarlinMr@users.noreply.github.com>
2021-01-16 13:02:01 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
-<?php
-if(!isset($_SERVER['HTTPS'])) {header("https://marlinmr.se/MarlinMr/wsb/index.php"); exit;}
-require "./head.html";
-require "./include/total.php";
-require "./include/table.php";
-require "./include/footer.php";
-?>
+<?php if(!isset($_SERVER['HTTPS'])) {header("https://marlinmr.se/MarlinMr/wsb/index.php"); exit;} ?>
+
+<div class="parent">
+<?php require "./head.html"; ?>
+<header class="blue section" contenteditable>
+  <?php require "./include/total.php"; ?>
+</header>
+<main class="coral section" contenteditable>
+  <?php require "./include/table.php"; ?>
+</main>
+<footer class="purple section" contenteditable>
+  <?php require "./include/footer.php"; ?>
+</footer>
+</div>
