673fbd9e50d10f3bf52c8fc4ce713e8824e9ed8b
MarlinMr <51798819+MarlinMr@users.noreply.github.com>
2021-01-16 11:32:57 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="./styles/styles.css?<?php echo date('l jS \of F Y h:i:s A'); ?>">
+        <script src="./scripts/main.js?<?php echo date('l jS \of F Y h:i:s A'); ?>"></script>
+        <meta charset="UTF-8">
+    </head>
+    <body onload="onLoad()">
+    </body>
+</html>
