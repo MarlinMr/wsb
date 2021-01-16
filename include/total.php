22dac72968977e73fb08c477e03995dfe8f605fd
MarlinMr <51798819+MarlinMr@users.noreply.github.com>
2021-01-16 12:43:02 +0100

    Update total.php

--- include/total.php
+++ include/total.php
@@ -1,15 +1,15 @@
 <?php
-  SELECT "SUM(portfolio) AS total FROM stocks WHERE stock =?";
+  $sql = "SELECT SUM(portfolio) AS total FROM stocks WHERE stock =?";
   require "connhandler.php";
   $stmt = $conn->prepare($sql);
   $stmt->bind_param("s", $stockid);
-  #$imgid = $_GET["stock"];
-  #$stmt->execute();
-  #$result = $stmt->get_result();
-  ##while ($row = $result->fetch_assoc()){
-  ##  foreach ($row as $r){
-  ##    print "$r ";
-  ##}
-  ##}
+  $imgid = $_GET["stock"];
+  $stmt->execute();
+  $result = $stmt->get_result();
+  while ($row = $result->fetch_assoc()){
+    foreach ($row as $r){
+      print "$r ";
+  }
+  }
   $conn->close();
 ?>
