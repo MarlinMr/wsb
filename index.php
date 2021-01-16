27fae3bbf3595b748e6fe8c3a37426e4a79937f6
MarlinMr <51798819+MarlinMr@users.noreply.github.com>
2021-01-16 11:51:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <link rel="stylesheet" href="./styles/styles.css?<?php echo date('l jS \of F Y h:i:s A'); ?>">
-        <script src="./scripts/main.js?<?php echo date('l jS \of F Y h:i:s A'); ?>"></script>
-        <meta charset="UTF-8">
-    </head>
-    <body onload="onLoad()">
-    </body>
-</html>
+<?php
+if(!isset($_SERVER['HTTPS'])) {header("https://marlinmr.se/MarlinMr/wsb/index.php"); exit;}
+require "include/total.php";
+require "include/table.php"
+require "include/footer.php"
+?>
