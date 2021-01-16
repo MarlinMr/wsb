71afd00b61cf40f6ef15a0088da8e7b455088c24
MarlinMr <51798819+MarlinMr@users.noreply.github.com>
2021-01-16 12:42:24 +0100

    Update total.php

--- include/total.php
+++ include/total.php
@@ -3,13 +3,13 @@
   require "connhandler.php";
   $stmt = $conn->prepare($sql);
   $stmt->bind_param("s", $stockid);
-  $imgid = $_GET["stock"];
-  $stmt->execute();
-  $result = $stmt->get_result();
-  #while ($row = $result->fetch_assoc()){
-  #  foreach ($row as $r){
-  #    print "$r ";
-  #}
-  #}
+  #$imgid = $_GET["stock"];
+  #$stmt->execute();
+  #$result = $stmt->get_result();
+  ##while ($row = $result->fetch_assoc()){
+  ##  foreach ($row as $r){
+  ##    print "$r ";
+  ##}
+  ##}
   $conn->close();
 ?>
