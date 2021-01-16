2627c093a2fb3f0ef791c1315c74859b9728f785
MarlinMr <51798819+MarlinMr@users.noreply.github.com>
2021-01-16 12:37:39 +0100

    Update total.php

--- include/total.php
+++ include/total.php
@@ -6,7 +6,7 @@ $stmt->bind_param("s", $stockid);
 $imgid = $_GET["stock"];
 $stmt->execute();
 $result = $stmt->get_result();
-while ($row = $result->fetch_array(MYSQLI_NUM)){
+while ($row = $result->fetch_assoc()){
   foreach ($row as $r){
     print "$r ";
   }
